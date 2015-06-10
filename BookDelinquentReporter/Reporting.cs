97c6c7c00d608077778706619d1a91533125e31d
Jesse Bernoudy <jbernoudy@salesforce.com>
2015-06-10 14:57:57 -0700

    Adding Reporting class and unit tests

---
+++ BookDelinquentReporter/Reporting.cs
@@ -0,0 +1,30 @@
+﻿using BookDelinquentReporter.Models;
+using BookDelinquentReporter.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDelinquentReporter
+{
+    public class Reporting
+    {
+        private ILibraryService _libraryService;
+
+        public Reporting(ILibraryService libraryService)
+        {
+            if(libraryService == null)
+            {
+                throw new ArgumentNullException("libraryService can not be null");
+            }
+            _libraryService = libraryService;
+        }
+
+        public async Task<int> GetNumberOfDeliquentMembers()
+        {
+            var delinquentMembers = await _libraryService.GetDelinquentMembers();
+            return delinquentMembers.Count;
+        }
+    }
+}
---
+++ Tests/ReportingTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookDelinquentReporter.Services;
+using BookDelinquentReporter.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookDelinquentReporter.Tests
+{
+    internal class LibraryServiceMock : ILibraryService
+    {
+        private List<Member> _delinquentMemebers;
+
+        public LibraryServiceMock(List<Member> delinquentMemebers)
+        {
+            _delinquentMemebers = delinquentMemebers;
+        }
+
+        public Task<List<Member>> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetAmountOwed(Member m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> GetDelinquentMembers()
+        {
+            return _delinquentMemebers;
+        }
+
+        Task<List<Member>> ILibraryService.GetDelinquentMembers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    [TestClass]
+    public class ReportingTest
+    {
+        private List<Member> _expectedDelinquentMembers;
+
+        public ReportingTest()
+        {
+            _expectedDelinquentMembers = new List<Member>()
+            {
+                new Member() { FirstName = "John", LastName = "Doe", Id = "1" },
+                new Member() { FirstName = "Jane", LastName = "Doe", Id = "2" },
+                new Member() { FirstName = "Tom", LastName = "Doe", Id = "3" },
+                new Member() { FirstName = "Dick", LastName = "Doe", Id = "4" },
+                new Member() { FirstName = "Harry", LastName = "Doe", Id = "5" },
+            };
+        }
+
+        [TestMethod]
+        public void TestReportingWithNullService()
+        {
+            try
+            {
+                Reporting reporting = new Reporting(null);
+                Assert.Fail("Expecting ArgumentNullException here.");
+            }
+            catch(Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentNullException));
+            }
+        }
+
+        [TestMethod]
+        public void TestReportingWith0DeliquentMembers()
+        {
+            Reporting reporting = new Reporting(new LibraryServiceMock(new List<Member>()));
+            Assert.AreEqual(0, reporting.GetNumberOfDeliquentMembers());
+        }
+
+        [TestMethod]
+        public void TestReportingWithKnownExpectedDeliquentMembers()
+        {
+            Reporting reporting = new Reporting(new LibraryServiceMock(_expectedDelinquentMembers));
+            Assert.AreEqual(_expectedDelinquentMembers.Count, reporting.GetNumberOfDeliquentMembers());
+        }
+
+        [TestMethod]
+        public void TestReportingGenerateReport()
+        {
+            Reporting reporting = new Reporting(new LibraryServiceMock(_expectedDelinquentMembers));
+        }
+    }
+}
